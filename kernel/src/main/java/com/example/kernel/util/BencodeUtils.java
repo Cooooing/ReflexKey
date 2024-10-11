4e4f80461fcbfdd08ee7bb7bdb83b65450d9fd34
Cooooing <2450144961@qq.com>
2024-10-11 23:04:28 +0800

    feat: add bencode utils

---
+++ kernel/src/main/java/com/example/kernel/util/BencodeUtils.java
@@ -0,0 +1,103 @@
+package com.example.kernel.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class BencodeUtils {
+
+    // 编码方法
+    public static String encode(Object obj) {
+        StringBuilder sb = new StringBuilder();
+        if (obj instanceof Map) {
+            sb.append("d");
+            Map<?, ?> map = (Map<?, ?>) obj;
+            for (Map.Entry<?, ?> entry : map.entrySet()) {
+                sb.append(encode(entry.getKey()));
+                sb.append(encode(entry.getValue()));
+            }
+            sb.append("e");
+        } else if (obj instanceof Integer) {
+            sb.append("i").append(obj).append("e");
+        } else if (obj instanceof List) {
+            sb.append("l");
+            List<?> list = (List<?>) obj;
+            for (Object item : list) {
+                sb.append(encode(item));
+            }
+            sb.append("e");
+        } else if (obj instanceof String) {
+            String str = (String) obj;
+            sb.append(str.length()).append(':').append(str);
+        } else {
+            throw new IllegalArgumentException("Unsupported type: " + obj.getClass());
+        }
+        return sb.toString();
+    }
+
+    // 解码方法
+    public static Object decode(byte[] s) {
+        return decodeObject(s, 0)[0];
+    }
+
+    // 辅助方法：解码对象
+    private static Object[] decodeObject(byte[] s, int index) {
+        byte b = s[index];
+        if (b == 'i') {
+            // 整数类型
+            index++;
+            int start = index;
+            while (s[index] != 'e') {
+                index++;
+            }
+            long value = Long.parseLong(new String(s, start, index - start));
+            index++;
+            return new Object[]{value, index};
+        } else if ('0' <= b && b <= '9') {
+            // 字符串类型
+            int start = index;
+            while (s[index] != ':') {
+                index++;
+            }
+            int length = Integer.parseInt(new String(s, start, index - start));
+            index++;
+            String str = new String(s, index, length);
+            index += length;
+            return new Object[]{str, index};
+        } else if (b == 'd') {
+            // 字典类型
+            index++;
+            Map<String, Object> map = new LinkedHashMap<>();
+            while (s[index] != 'e') {
+                Object[] keyResult = decodeObject(s, index);
+                String key = (String) keyResult[0];
+                index = (int) keyResult[1];
+                Object[] valueResult = decodeObject(s, index);
+                Object value = valueResult[0];
+                index = (int) valueResult[1];
+                map.put(key, value);
+            }
+            index++;
+            return new Object[]{map, index};
+        } else if (b == 'l') {
+            // 列表类型
+            index++;
+            List<Object> list = new ArrayList<>();
+            while (s[index] != 'e') {
+                Object[] result = decodeObject(s, index);
+                list.add(result[0]);
+                index = (int) result[1];
+            }
+            index++;
+            return new Object[]{list, index};
+        } else {
+            throw new IllegalArgumentException("Invalid Bencode format at index " + index);
+        }
+    }
+}
+
+
