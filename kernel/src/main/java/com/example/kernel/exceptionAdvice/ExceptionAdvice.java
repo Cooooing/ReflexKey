42a7f03e25cd645cca306a363a576b2c03ff03ea
Cooooing <2450144961@qq.com>
2024-10-11 10:43:52 +0800

    perf: optimize development environment error handling

--- kernel/src/main/java/com/example/kernel/exceptionAdvice/ExceptionAdvice.java
+++ kernel/src/main/java/com/example/kernel/exceptionAdvice/ExceptionAdvice.java
@@ -3,6 +3,7 @@ package com.example.kernel.exceptionAdvice;
 import com.example.kernel.entity.base.Result;
 import com.example.kernel.entity.enums.ExceptionEnum;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
@@ -11,9 +12,15 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 @RestControllerAdvice
 public class ExceptionAdvice {
 
+    @Value("${spring.profiles.active}")
+    private String active;
+
     @ExceptionHandler(Exception.class)
     public Result<Object> exceptionAdvice(Exception e) {
         log.error(e.getMessage(), e);
+        if ("dev".equals(active)) {
+            return Result.error(e);
+        }
         return Result.error(ExceptionEnum.INTERNAL_SERVER_ERROR);
     }
 
