9797f83ec354a9f1fafb6d95f0939b64772a8fb1
csfwff <csfwff@163.com>
2024-11-18 15:07:40 +0800

    增加tailwind

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Message from "@/components/Message";
+import "./main.css";
 
 const app = createApp(App);
 app.use(store);
