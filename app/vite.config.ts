979d5d1d91e1ccb537c504a8178f3dc9bfc1ef59
Cooooing <2450144961@qq.com>
2024-11-21 19:56:55 +0800

    change to vite and pnpm

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
-
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,20 +12,20 @@ export default defineConfig({
     vue(),
     AutoImport({
       imports: [
-     'vue'ue",
+        "vue",
         {
-       'naive-ui'ui": [
-         'useDialog'og",
-         'useMessage'ge",
-         'useNotification'on",
-         'useLoadingBar'r",
-         ],
-       },
-     ],
+          "naive-ui": [
+            "useDialog",
+            "useMessage",
+            "useNotification",
+            "useLoadingBar",
+          ],
+        },
+      ],
     }),
     Components({
-      resolvers: [NaiveUiResolver)],
-   }),
+      resolvers: [NaiveUiResolver()],
+    }),
   ],
   resolve: {
     alias: {
